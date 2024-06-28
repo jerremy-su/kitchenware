781cc12be77be84b1fe54b531d533d2d88e834f0
jerremy.su <jerremy.su@shijigroup.com>
2024-06-28 21:16:45 +0800

    2024-06-28 21:16:43 save code

--- kitchenware/src/main/java/org/kitchenware/spring/web/hook/ServiceInvokerIterator.java
+++ kitchenware/src/main/java/org/kitchenware/spring/web/hook/ServiceInvokerIterator.java
@@ -1,9 +1,36 @@
 package org.kitchenware.spring.web.hook;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.kitchenware.express.annotation.NotNull;
+import org.kitchenware.object.transport.rpc.flow.ObjectSerialize;
 
 public interface ServiceInvokerIterator {
 
+	static final Logger LOGGER = Logger.getLogger(ServiceInvokerIterator.class.getName());
+	
 	boolean invokeNext(
-			@NotNull ServiceHook hook) throws Exception;
+			@NotNull final ServiceHook hook) throws Exception;
+	
+	default boolean throwErrorReponse(
+			@NotNull final ServiceHook hook, @NotNull final Throwable caughtError) throws Exception{
+		
+		ServiceInvokerResult result = new ServiceInvokerResult()
+				.setCaughtError(caughtError)
+				;
+		
+		try {
+			HttpServletResponse response = hook.httpResponse();
+			response.setContentType("application/octet-stream");
+			ObjectSerialize serialize = new ObjectSerialize(result);
+			serialize.writeObject(hook.httpResponse().getOutputStream());
+			response.flushBuffer();
+			return true;
+		} catch (Throwable e) {
+			throw new IOException(e.getMessage(), e);
+		}
+	}
 }
