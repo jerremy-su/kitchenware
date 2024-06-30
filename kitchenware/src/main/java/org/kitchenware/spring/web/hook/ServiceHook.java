d49ecdc775838e7e9f32d420d16cf66afdb6571e
jerremy.su <jerremy.su@shijigroup.com>
2024-06-30 15:06:22 +0800

    2024-06-30 15:06:20 save code

--- kitchenware/src/main/java/org/kitchenware/spring/web/hook/ServiceHook.java
+++ kitchenware/src/main/java/org/kitchenware/spring/web/hook/ServiceHook.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.kitchenware.express.annotation.NotNull;
+import org.kitchenware.express.annotation.Required;
 import org.springframework.context.ApplicationContext;
 
 public interface ServiceHook {
@@ -17,4 +18,50 @@ public interface ServiceHook {
 	@NotNull 
 	ApplicationContext applicationContext();
 	
+	
+	static ServiceHook build(
+			@NotNull final HttpServletRequest request
+			, @NotNull final HttpServletResponse response
+			, @NotNull final ApplicationContext applicationContext
+			) {
+		return new DefaultServiceHook(request, response, applicationContext);
+	}
+	
+	static class DefaultServiceHook implements ServiceHook{
+		
+		@Required
+		final HttpServletRequest request;
+		
+		@Required
+		final HttpServletResponse response;
+		
+		@Required
+		final ApplicationContext applicationContext;
+		
+		DefaultServiceHook(
+				@NotNull final HttpServletRequest request
+				, @NotNull final HttpServletResponse response
+				, @NotNull final ApplicationContext applicationContext
+				){
+			this.request = request;
+			this.response = response;
+			this.applicationContext = applicationContext;
+		}
+
+		@Override
+		public @NotNull HttpServletRequest httpRequest() {
+			return this.request;
+		}
+
+		@Override
+		public @NotNull HttpServletResponse httpResponse() {
+			return this.response;
+		}
+
+		@Override
+		public @NotNull ApplicationContext applicationContext() {
+			return this.applicationContext;
+		}
+		
+	}
 }
--- kitchenware/src/test/java/org/kitchenware/spring/web/hook/ServiceHookController.java
+++ kitchenware/src/test/java/org/kitchenware/spring/web/hook/ServiceHookController.java
@@ -1,11 +1,15 @@
 package org.kitchenware.spring.web.hook;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.kitchenware.express.annotation.NotNull;
-import org.kitchenware.express.io.ByteBufferedOutputStream;
-import org.kitchenware.express.io.IOSteramLoader;
+import org.kitchenware.object.transport.rpc.flow.ObjectDeserialize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,16 +20,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/test/rpc/hook")
 public class ServiceHookController {
 
+	static final Logger LOGGER = Logger.getLogger(ServiceHookController.class.getName());
+	
 	@Autowired
 	ApplicationContext applicationContext;
 	
 	
 	@PostMapping("/invoke")
 	public void invoke(
-			@NotNull HttpServletRequest httpRequest, @NotNull HttpServletResponse respnose) throws Exception{
-		ByteBufferedOutputStream buf = new ByteBufferedOutputStream();
-		IOSteramLoader.load(httpRequest.getInputStream(), buf);
+			@NotNull HttpServletRequest httpRequest, @NotNull HttpServletResponse response) throws Exception{
+		ServiceInvokeIterator iterator = readIterator(httpRequest.getInputStream());
+		ServiceHook serviceHook = ServiceHook.build(httpRequest, response, applicationContext);
+		iterator.invokeNext(serviceHook);
+		response.flushBuffer();
+	}
+	
+	private ServiceInvokeIterator readIterator(
+			@NotNull InputStream inputStream
+			) throws Exception{
+		ServiceInvokeIterator loadIterator = null;
+		Throwable caughtError = null;
 		
-		//TODO
+		try {
+			ObjectDeserialize deserializer = new ObjectDeserialize(inputStream);
+			Object loadObject = deserializer.readObject();
+			if(! ServiceInvokeIterator.class.isInstance(loadObject)) {
+				throw new IllegalAccessException(String.format("Invalid transport type"));
+			}
+			loadIterator = ServiceInvokeIterator.class.cast(loadObject);
+		} catch (Throwable e) {
+			caughtError = e;
+			LOGGER.log(Level.WARNING, e.getMessage(), e);
+		}
+		
+		if(caughtError != null) {
+			throw new IOException("Invalid Request", caughtError);
+		}
+		
+		return loadIterator;
 	}
 }
